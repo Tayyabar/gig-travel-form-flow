596791f49cb2b01a806886daa68f336c726ac536
Tayyab Abdul Rauf <tayyab_110055@live.com>
2022-05-08 09:21:45 +0300

    remote-app-test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ import "./assets/css/style.css";
 import "./assets/css/custom_en.css";
 import "./assets/css/bod.css";
 import "./assets/css/all.css";
+class WebComponent extends HTMLElement {
+  connectedCallback() {
+    ReactDOM.render(<App />, this);
+  }
+}
+
+const ELEMENT_ID = "gig-remote-app";
+
+if (!customElements.get(ELEMENT_ID)) {
+  customElements.define(ELEMENT_ID, WebComponent);
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
